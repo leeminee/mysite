0b1aca6a4f53fda5dedea8d8be2dc7b9cb1f286e
minee <mingramer@gmail.com>
2022-12-01 17:55:39 +0900

    튜토리얼2까지 따라하며 데이터베이스 SQLite로 연동

--- polls/apps.py
+++ polls/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
+# mysite/settings.py 파일에 INSTALLED_APPS 설정에 아래와 같이 들어가야 한다.
+# polls.apps.PollsConfig
 
 class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
