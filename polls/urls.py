b2874262c0f8309fc3da7dbaa5b5775cf0fa3a98
minee <mingramer@gmail.com>
2022-12-01 15:14:39 +0900

    Django 튜토리얼1 따라하면서 모르는 것 주, ìì석ë, 설명 등 주석 남김

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+# view 를 호출 하기 위해 생성한 URLconf 파일이다.
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # 💡 polls/views.py 의 index 함수로 간다는 뜻?
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,7 @@
+from venv import logger
+
 from django.http import HttpResponse
 
 def index(request):
+    logger.info('request : ', request) # 💡 왜 이 로그가 안찍히지?
     return HttpResponse("Hello, world. You're at the polls index.")
