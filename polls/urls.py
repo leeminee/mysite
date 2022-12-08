7b10ab794ed46ebfb5092135b2431f0fe76f7c1d
minee <mingramer@gmail.com>
2022-12-08 16:03:44 +0900

    튜토리얼4까지 실습

--- polls/urls.py
+++ polls/urls.py
@@ -7,11 +7,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'), # 💡 polls/views.py 의 index 함수로 간다는 뜻?
+    path('', views.IndexView.as_view(), name='index'), 
     # ex: /polls/5/
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='result'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
